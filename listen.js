bb682717a1f743534cd109f81eeab20e3afdc00d
JockLee <5155291+webees@users.noreply.github.com>
2024-04-21 02:39:27 +0800

    Update listen.js

--- listen.js
+++ listen.js
@@ -3,7 +3,7 @@ const http = require("http");
 const PORT = process.env.PORT || 8080;
 (async function main() {
   try {
-    const signer = new Signer();
+    const signer = new Signer("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edge/107.0.1418.56");
     const start = new Date();
 
     const server = http
